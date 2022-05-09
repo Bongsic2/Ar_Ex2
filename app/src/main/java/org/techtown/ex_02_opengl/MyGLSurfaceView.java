d9d1f65e93fc3a3c80d6de6c00fb38821c07df19
bongsic <dlwjdsmd44@naver.com>
2022-05-09 21:21:51 +0900

    ar2

---
+++ app/src/main/java/org/techtown/ex_02_opengl/MyGLSurfaceView.java
@@ -0,0 +1,19 @@
+package org.techtown.ex_02_opengl;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MyGLSurfaceView extends GLSurfaceView {
+
+
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(2);
+
+        setRenderer(new MyGLRenderer(context));
+
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+}
